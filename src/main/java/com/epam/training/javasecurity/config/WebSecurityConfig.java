d4514fee074a6a7b76cb082e9c8f3c38ffe1037d
Peter Veres <peter_veres2@epam.com>
2016-11-02 16:46:41 +0100

    Custom login form

--- src/main/java/com/epam/training/javasecurity/config/WebSecurityConfig.java
+++ src/main/java/com/epam/training/javasecurity/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.epam.training.javasecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,9 +26,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //	}
 
 	
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception {
 		auth.userDetailsService(userDetailsService);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+
+		http
+		.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+		.formLogin().loginPage("/login")
+			.permitAll()
+			.and()
+		.logout()
+			.permitAll();
 	}
 	
 
